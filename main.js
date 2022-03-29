e94b0f4e9fe7769b704ee3dceb0973ed12f3f04f
Marvin-Erazo <Marvin-Erazo@hotmail.com>
2022-03-29 16:16:02 -0500

    design finished

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
-function changeColor(e){
 
+function changeColor(e){
+  selectedRate();
+  let btn = e.target;
+  btn.className = btn.className == "selected" ? "rate-btn":"selected";
+}
+
+function selectedRate(){
   let selectedBtns = document.querySelectorAll('.selected');
-  
   if(selectedBtns.length>0){
     selectedBtns.forEach(element => {
       element.className = "rate-btn"
     });
   }
+}
 
-  let btn = e.target;
+function verify(){
+  let selectedBtns = document.querySelectorAll('.selected');
+  if(selectedBtns.length === 0){
+    alert("select a rate");
+  }
+  else{
+    showThanks();
+  }
+}
 
-  btn.className = btn.className == "selected" ? "rate-btn":"selected";
+function showThanks(){
+  let ratecard = document.getElementsByClassName("rate-card");
+  ratecard[0].style.display = "none";
+
+  let modal = document.getElementsByClassName("modal")[0].style.display = "flex";
+  
+  document.getElementsByClassName("rate")[0].innerHTML = "You selected "+ document.getElementsByClassName("selected")[0].value +" out of 5";
 }
